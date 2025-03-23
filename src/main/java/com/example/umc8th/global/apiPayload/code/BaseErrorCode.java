f2293c294c5bba557d9c87dc56dd20ab338b8281
Jimyeong Kim <kjm021221@icloud.com>
2025-03-23 23:48:32 +0900

    :sparkles:feat: 응답 통일, 예외처리, 에러 핸들러 작성

---
+++ src/main/java/com/example/umc8th/global/apiPayload/code/BaseErrorCode.java
@@ -0,0 +1,15 @@
+package com.example.umc8th.global.apiPayload.code;
+
+import com.example.umc8th.global.apiPayload.CustomResponse;
+import org.springframework.http.HttpStatus;
+
+public interface BaseErrorCode {
+
+    HttpStatus getHttpStatus();
+    String getCode();
+    String getMessage();
+
+    default CustomResponse<Void> getErrorResponse() {
+        return CustomResponse.onFailure(getCode(), getMessage());
+    }
+}
