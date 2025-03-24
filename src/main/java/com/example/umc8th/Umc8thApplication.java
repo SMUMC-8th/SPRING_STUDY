5766060abe5dfaafcf509cbddb9ffe93e352ebd5
noonbbara <ysys1226@naver.com>
2025-03-24 15:22:46 +0900

    🚀 chore: commit test

--- src/main/java/com/example/umc8th/Umc8thApplication.java
+++ src/main/java/com/example/umc8th/Umc8thApplication.java
@@ -3,7 +3,6 @@ package com.example.umc8th;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-// git test
 @SpringBootApplication
 public class Umc8thApplication {
 
