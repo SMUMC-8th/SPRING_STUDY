a9ffe9cac72dbc302c23ffed7d301de16a3621fc
hyoin <hyoinyang02@gmail.com>
2025-03-27 20:30:49 +0900

    ✨ feat: 에러 핸들러

--- src/main/java/com/example/umc8th/global/apiPayload/code/BaseErrorCode.java
+++ src/main/java/com/example/umc8th/global/apiPayload/code/BaseErrorCode.java
@@ -3,7 +3,7 @@ package com.example.umc8th.global.apiPayload.code;
 import org.springframework.http.HttpStatus;
 
 public interface BaseErrorCode {
-    HttpStatus getStatus();
+    HttpStatus getHttpStatus();
     String getCode();
     String getMessage();
 }
