3d262108307e50fdf86e9d031e98e556f01684bc
noonbbara <ysys1226@naver.com>
2025-04-12 18:19:30 +0900

    ✨ feat: id를 이용한 커서 기반 article 페이지네이션 (아직 구현 중입니다...)

--- src/main/java/com/example/umc8th/domain/article/service/query/ArticleQueryService.java
+++ src/main/java/com/example/umc8th/domain/article/service/query/ArticleQueryService.java
@@ -1,5 +1,6 @@
 package com.example.umc8th.domain.article.service.query;
 
+import com.example.umc8th.domain.article.dto.ArticleResponseDTO;
 import com.example.umc8th.domain.article.entity.Article;
 
 import java.util.List;
@@ -7,4 +8,6 @@ import java.util.List;
 public interface ArticleQueryService {
     Article getArticle(Long id);
     List<Article> getArticles();
+    ArticleResponseDTO.PageArticleDTO getPageArticlesCursorById(int size, Long cursor);
+
 }
