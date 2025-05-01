5ab840259446e3eb784ef77c29b78f3f65be4e8b
Juheon <juheon0313@naver.com>
2025-05-01 21:41:47 +0900

    🔨 [refactor] import문 최적화

--- src/main/java/com/example/umc8th/global/auth/filter/JwtFilter.java
+++ src/main/java/com/example/umc8th/global/auth/filter/JwtFilter.java
@@ -9,12 +9,10 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
