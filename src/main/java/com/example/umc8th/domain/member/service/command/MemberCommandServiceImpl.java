e1e275034ab7ad4df2c8180506e4c4c80c235105
Juheon <juheon0313@naver.com>
2025-05-01 21:35:53 +0900

    ✨ [feat] Member 로그인 기능 구현

--- src/main/java/com/example/umc8th/domain/member/service/command/MemberCommandServiceImpl.java
+++ src/main/java/com/example/umc8th/domain/member/service/command/MemberCommandServiceImpl.java
@@ -3,7 +3,11 @@ package com.example.umc8th.domain.member.service.command;
 import com.example.umc8th.domain.member.converter.MemberConverter;
 import com.example.umc8th.domain.member.dto.MemberRequestDTO;
 import com.example.umc8th.domain.member.dto.MemberResponseDTO;
+import com.example.umc8th.domain.member.entity.Member;
+import com.example.umc8th.domain.member.exception.MemberException;
+import com.example.umc8th.domain.member.exception.code.MemberErrorCode;
 import com.example.umc8th.domain.member.repository.MemberRepository;
+import com.example.umc8th.global.auth.service.command.TokenCommandService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -14,6 +18,7 @@ public class MemberCommandServiceImpl implements MemberCommandService {
 
     private final MemberRepository memberRepository;
     private final PasswordEncoder passwordEncoder;
+    private final TokenCommandService tokenCommandService;
 
 
     @Override
@@ -24,5 +29,15 @@ public class MemberCommandServiceImpl implements MemberCommandService {
                         MemberConverter.toMember(dto.username(), password)
                 )
         );
+    }
+
+    @Override
+    public MemberResponseDTO.LoginResponseDTO login(MemberRequestDTO.LoginRequestDTO dto) {
+        Member member = memberRepository.findByUsername(dto.username()).orElseThrow(() ->
+                new MemberException(MemberErrorCode.NOT_FOUND_404));
+        if (!passwordEncoder.matches(dto.password(), member.getPassword())) {
+            throw new MemberException(MemberErrorCode.BAD_REQUEST_400);
+        }
+        return tokenCommandService.createLoginToken(member); // 유저 정보로 토큰만들기, 참고로 DTO에는 id, accessToken, refreshToken이 존재합니다.
     }
 }
