8edcbd216390822c24f7e38dd1f4c3eccbd0b570
hyoin <hyoinyang02@gmail.com>
2025-05-06 09:56:49 +0900

    ✨ [feat] member에서 JWT 토큰 생성

--- src/main/java/com/example/umc8th/controller/MemberController.java
+++ src/main/java/com/example/umc8th/controller/MemberController.java
@@ -6,10 +6,7 @@ import com.example.umc8th.entity.Member;
 import com.example.umc8th.global.apiPayload.CustomResponse;
 import com.example.umc8th.service.command.MemberCommandService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
@@ -22,5 +19,11 @@ public class MemberController {
     public CustomResponse<MemberResponseDTO.SignUpResponseDTO> signUp(@RequestBody MemberRequestDTO.SignUpRequestDTO dto) {
         Member member = memberCommandService.signUp(dto);
         return CustomResponse.ok(MemberResponseDTO.SignUpResponseDTO.from(member));
+    }
+
+    @GetMapping("/login")
+    public CustomResponse<MemberResponseDTO.LoginResponseDTO> login(@RequestBody MemberRequestDTO.LoginRequestDTO dto) {
+        MemberResponseDTO.LoginResponseDTO member = memberCommandService.login(dto);
+        return CustomResponse.ok(member);
     }
 }
